d632fc9a2141e8bc66448ae0fd94e4db3997f2d4
zoyaasif <zoya021asif@gmail.com>
2026-08-23 22:56:27 +0530

    Positive,Negative or Zero.

---
+++ Question6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    if(n % 2 == 0){
+        cout<<"The number is even";
+    }
+    else{
+        cout<<"The number is odd";
+    }
+    return 0;
+}
---
+++ Question7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter three numbers:";
+    cin>> a >> b >> c;
+    if(a > b && a > c){
+        cout<<"Largest "<<a;
+    }
+    else if(b > a && b > c){
+        cout<<"Largest "<<b;
+    }
+    else{
+        cout<<"Largest "<<c;
+    }
+    return 0;
+}
