f8a82fb1692cd811cae348314af5699e1a653004
zoyaasif <zoya021asif@gmail.com>
2026-08-23 23:34:43 +0530

    Sum of two numbers.

---
+++ Question9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter two number:";
+    cin>> num1 >> num2;
+    int sum = 0;
+    sum = num1 + num2;
+    cout<<"sum = "<< sum << endl;
+    return 0;
+}
